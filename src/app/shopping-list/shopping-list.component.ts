4f677aa750439c07cadf9bc610256e1a4ab47ae5
ApiSupak <apiwat@sirisoft.co.th>
2022-08-20 22:59:03 +0700

    finishing section 5

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients:Ingredients[] = [
+    new Ingredients('Apple',5),
+    new Ingredients('Orange',3)
+  ];
 
   constructor() { }
 
