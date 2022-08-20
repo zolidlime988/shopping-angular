96c440d572fc432182ba5519fed5234af792bcad
Chin <api.supak@gmail.com>
2022-08-20 21:18:24 +0700

    save components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ListComponentComponent } from './list-component/list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ListComponentComponent
   ],
   imports: [
     BrowserModule
